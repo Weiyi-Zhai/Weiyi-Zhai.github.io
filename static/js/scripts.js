6a8babb6e020ce33b6498e6099198d3969273087
Weiyi-Zhai <70766962+Weiyi-Zhai@users.noreply.github.com>
2024-12-09 19:05:53 -0500

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards']
+const section_names = ['home', 'publications', 'teaching', 'presentations']
 
 
 window.addEventListener('DOMContentLoaded', event => {
